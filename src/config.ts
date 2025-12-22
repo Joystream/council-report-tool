6b9d9c334669cc2b59fdbd4a449aab95f7f3de34
l1.media <dev@l1.media>
2025-12-22 13:40:26 +0100

    Make Imgur Client ID configurable

--- src/config.ts
+++ src/config.ts
@@ -5,6 +5,7 @@ export const ORION_URL =
 export const NODE_SOCKET =
   import.meta.env.VITE_NODE_SOCKET || "wss://rpc.joystream.org:9944";
 export const FEE_QN_URL = "https://monitoring.joyutils.org/fees/graphql";
+export const IMGUR_CLIENT_ID = ""
 export const MEXC_WALLET = "5CFYPwkpP4oEeCUviVafWBNwpRPF7SjUpL4q8A7KtJJhQ1gJ";
 export const defaultDateTimeFormat = "DD MMMM YYYY";
 export const proposalLink = "https://pioneerapp.xyz/#/proposals/preview/";
